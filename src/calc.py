556a06888d2a31a33bc520a20d64c348a9302b14
Alexd32 <dementev.aleksei@gmail.com>
2025-08-03 10:22:43 +0300

    copilod edit

--- src/calc.py
+++ src/calc.py
@@ -5,3 +5,4 @@ class calc:
     
     def sum(x, y):
         return x + y
+    
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -26,3 +26,38 @@ class TestCalc:
     )
     def test_sum(self, x, y, rez):
         assert calc.sum(x, y) == rez
+        @pytest.mark.parametrize(
+            "x, y, expected",
+            [
+                (2, 3, 5),
+                (-1, 1, 0),
+                (0, 0, 0),
+                (100, 200, 300),
+            ]
+        )
+        def test_sum_additional(x, y, expected):
+            assert calc.sum(x, y) == expected
+
+        @pytest.mark.parametrize(
+            "x, y, expected",
+            [
+                (6, 3, 2),
+                (10, 2, 5),
+                (-4, 2, -2),
+                (0, 1, 0),
+            ]
+        )
+        def test_delen_additional(x, y, expected):
+            assert calc.delen(x, y) == expected
+
+        @pytest.mark.parametrize(
+            "x, y",
+            [
+                (1, 0),
+                (0, 0),
+                (-5, 0),
+            ]
+        )
+        def test_delen_zero_division(x, y):
+            with pytest.raises(ZeroDivisionError):
+                calc.delen(x, y)
